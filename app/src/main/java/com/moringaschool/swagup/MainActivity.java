f53081af23a30c14fe44109ddfea0c18472edf8e
kephanyamweya <kepha.nyamweya@student.moringaschool.com>
2021-09-06 03:56:25 +0300

    add a toast to the project

--- app/src/main/java/com/moringaschool/swagup/MainActivity.java
+++ app/src/main/java/com/moringaschool/swagup/MainActivity.java
@@ -3,12 +3,23 @@ package com.moringaschool.swagup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindAttiresButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindAttiresButton = (Button) findViewById(R.id.findAttiresButton);
+        mFindAttiresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "swag up!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
