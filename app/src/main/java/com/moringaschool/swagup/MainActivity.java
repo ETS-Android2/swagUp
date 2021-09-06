ee009b6b3f2d4ea423a5206c75c0908d2a478d70
kephanyamweya <kepha.nyamweya@student.moringaschool.com>
2021-09-06 04:09:37 +0300

    add intent to navigate to DressUp activity

--- app/src/main/java/com/moringaschool/swagup/MainActivity.java
+++ app/src/main/java/com/moringaschool/swagup/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.swagup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "swag up!",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this,DressUp.class);
+                startActivity(intent);
             }
         });
     }
