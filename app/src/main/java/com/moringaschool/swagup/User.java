051a3309454a575b5b0993fada67f67e20040bc2
kephanyamweya <kepha.nyamweya@student.moringaschool.com>
2021-09-07 11:05:20 +0300

    add new activity (listview)to the project to handle the images and descriptions

---
+++ app/src/main/java/com/moringaschool/swagup/User.java
@@ -0,0 +1,12 @@
+package com.moringaschool.swagup;
+
+public class User {
+    String name,message;
+    int imageid;
+
+    public User(String name, String message, int imageid) {
+        this.name = name;
+        this.message = message;
+        this.imageid = imageid;
+    }
+}
