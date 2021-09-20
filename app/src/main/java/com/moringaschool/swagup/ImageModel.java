6d25c26df70aa8eca71b1ab00f63ff5ed1b5d4c5
kephanyamweya <kepha.nyamweya@student.moringaschool.com>
2021-09-20 14:14:35 +0300

    add imagesmodel from the api

---
+++ app/src/main/java/com/moringaschool/swagup/ImageModel.java
@@ -0,0 +1,50 @@
+package com.moringaschool.swagup;
+
+import retrofit2.http.PUT;
+
+public class ImageModel {
+    private int id;
+    private String originalUrl,mediumUrl,photographerName;
+
+    public ImageModel(){
+
+    }
+    public ImageModel(int id, String originalUrl, String mediumUrl, String photographerName){
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.mediumUrl = mediumUrl;
+        this.photographerName = photographerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getMediumUrl() {
+        return mediumUrl;
+    }
+
+    public void setMediumUrl(String mediumUrl) {
+        this.mediumUrl = mediumUrl;
+    }
+
+    public String getPhotographerName() {
+        return photographerName;
+    }
+
+    public void setPhotographerName(String photographerName) {
+        this.photographerName = photographerName;
+    }
+}
