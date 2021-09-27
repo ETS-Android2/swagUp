9ce9732cf797b1a4e55aed27e3594e3d46a79cfa
kephanyamweya <kepha.nyamweya@student.moringaschool.com>
2021-09-27 10:22:57 +0300

    add constants to handle the api key

---
+++ app/src/main/java/com/moringaschool/swagup/constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.swagup;
+
+public class constants {
+    public static final String API_KEY ="563492ad6f91700001000001fbc5d384685a46609f2807fe967b7787";
+    public static final String BASE_URL ="https://api.pexels.com/v1/";
+}
